a905be59dd7430db015856b3711a4f89324679d0
Hamzakh3 <hamkh3@gmail.com>
2020-12-27 19:57:41 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,129 @@
+const cloud = [document.querySelector(".cloud"), 
+document.querySelector(".cloud1"),
+document.querySelector(".cloud2"),
+document.querySelector(".eagle"),
+document.querySelector(".sparrow"),
+document.querySelector(".plane"),
+document.querySelector(".bar1"),
+
+
+]
+// const plane = 
+
+
+const clouds = [
+    {
+        id:'cloud',
+        frame: [
+            { transform: 'translateX(10vw)', opacity: 0.3 },
+            { transform: 'translateX(-100vw)', opacity: 1 },
+        ],
+        timing: {
+            duration: 10000,
+            iterations: Infinity,
+        }
+    },
+    {
+        id:'cloud1',
+        frame: [
+            { transform: 'translateX(5vw)', opacity: 0.4 },
+            { transform: 'translateX(-100vw)', opacity: 1 },
+        ],
+        timing: {
+            duration: 10000,
+            iterations: Infinity,
+        }
+    },{
+        id:'cloud2',
+        frame: [
+            { transform: 'translateX(15vw)', opacity: 0.4 },
+            { transform: 'translateX(-90vw)', opacity: 1 },
+        ],
+        timing: {
+            duration: 10000,
+            iterations: Infinity,
+        }
+    },
+    {
+        id:'eagle',
+        frame: [
+            { transform: 'translateX(5vw)', opacity: 1 },
+            { transform: 'translateX(0vw)', opacity: 1 },
+        ],
+        timing: {
+            duration: 20000,
+            iterations: Infinity,
+        }
+    },{
+        id:'sparrow',
+        frame: [
+            { transform: 'translateX(25vw)', opacity: 1 },
+            { transform: 'translateX(-22vw)', opacity: 1 },
+        ],
+        timing: {
+            duration: 20000,
+            iterations: Infinity,
+        }
+    },
+    {
+        id:'plane',
+        frame: [
+            { transform: 'translate(0px, 20px)', opacity: 1 },
+            { transform: 'translate(500px, 720px)', opacity: 1 },
+            { transform: 'translate(800px, 720px)', opacity: 1 },
+
+        ],
+        timing: {
+            duration: 16000,
+            iterations: Infinity,
+        }
+    },
+    {
+        id:'bar1',
+        frame: [
+            { transform: 'translateX(10px)', opacity: 1 },
+            { transform: 'translateX(-100vw)', opacity: 1 },
+            
+        ],
+        timing: {
+            duration: 16000,
+            iterations: Infinity,
+        }
+    },
+    // {
+    //     id:'bar2',
+    //     frame: [
+    //         { transform: 'translateX(10px)', opacity: 1 },
+    //         { transform: 'translateX(-100vw)', opacity: 1 },
+    //     ],
+    //     timing: {
+    //         duration: 16000,
+    //         iterations: Infinity,
+    //     }
+    // },
+    // {
+    //     id:'bar3',
+    //     frame: [
+    //         { transform: 'translateX(10px)', opacity: 1 },
+    //         { transform: 'translateX(-100vw)', opacity: 1 },
+    //     ],
+    //     timing: {
+    //         duration: 6000,
+    //         iterations: Infinity,
+    //     }
+    // }
+]
+
+// const cloudFrame = [
+//     { transform: 'translateX(10vw)', opacity: 0.8 },
+//     { transform: 'translateX(-600px)', opacity: 1 },
+// ]
+
+// const cloudTiming = {
+
+// }
+
+// cloud.animate(cloudFrame, cloudTiming)
+clouds.map((obj, index)=>{
+    cloud[index].animate(obj.frame, obj.timing)
+})
